54cd4f4c7b270886faa9f3b664c88e06101c2534
Avinash Vishwakarma <avnsh786karma@gmail.com>
2018-02-19 01:41:40 +0530

    checkbox material reference in material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core'
-import { MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material'
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCheckboxModule } from '@angular/material'
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule]
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCheckboxModule]
 })
 
 export class MaterialModule {}
